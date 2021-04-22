ada2a1c9a45e7cf4ff2905fb1f874d425491518b
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2021-04-22 17:38:01 +0100

    Updated routes with new folder structure, created services page and link to it

---
+++ src/app/dashboard/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const express = require('express');
+// const logger = require('../../infrastructure/logger');
+const { asyncWrapper } = require('login.dfe.express-error-handling');
+
+const { get: getDashboard } = require('./dashboard');
+
+const router = express.Router({ mergeParams: true });
+
+const routes = (csrf) => {
+
+  router.get('/', csrf, asyncWrapper(getDashboard));
+
+  // add all other routes to new pages here
+
+  return router;
+};
+
+module.exports = routes;
