eb7f8d8158f9fe8e7b2951ab21b5147c9e9d1a0c
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2021-04-23 13:29:50 +0100

    Using a template with EJS logic and then passing in content to it

--- src/app/dashboard/dashboard.js
+++ src/app/dashboard/dashboard.js
@@ -2,6 +2,70 @@ const get = async (req, res) => {
   const model = {
     csrfToken: req.csrfToken(),
     title: 'DfE Sign-in help',
+    header: 'Find help with...',
+    description: 'Choose the main topic for which you need assistance. Each category contains articles designed to help with specific questions.',
+    sections: [
+      {
+        title: 'Services',
+        link: '/services',
+        text: 'Find help with services, including how to add or request a service, and what to do when a service is unavailable.',
+      },
+      {
+        title: 'Organisations',
+        link: '/services',
+        text: 'Find help with organisations, including how to request access to an organisation, and how to add multiple organisations to a single account.',
+      },
+      {
+        title: 'Manage users',
+        link: '/services',
+        text: 'Find help with manage users, including how to invite new users, and how to add a service to a user.',
+      },
+      {
+        title: 'Requests',
+        link: '/services',
+        text: 'Find help with requests, including how to raise a request, and how to either approve or reject the requests that you manage.',
+      },
+      {
+        title: 'Profile',
+        link: '/services',
+        text: 'Find help with your profile, including how to change your email address and password.',
+      },
+      {
+        title: 'Approvers',
+        link: '/services',
+        text: 'Find help with approver-related questions, including what an approver is and what they do.',
+      },
+      {
+        title: 'End users',
+        link: '/services',
+        text: 'Find help with end user-related questions, including how to add a service to an end user.',
+      },
+    ],
+    relatedArticles: {
+      title: 'Most popular help topics',
+      items: [
+        {
+          text: 'How do I approve a request?',
+          link: '/services',
+        },
+        {
+          text: 'What is an approver?',
+          link: '/services',
+        },
+        {
+          text: 'How do I request access to a service?',
+          link: '/services',
+        },
+        {
+          text: 'How do I add a service to my account?',
+          link: '/services',
+        },
+        {
+          text: 'Do I need a separate email address for each organisation?',
+          link: '/services',
+        },
+      ],
+    },
   };
   return res.render('dashboard/views/dashboard', model);
 };
--- src/app/services/services.js
+++ src/app/services/services.js
@@ -2,8 +2,79 @@ const get = async (req, res) => {
   const model = {
     csrfToken: req.csrfToken(),
     title: 'DfE Sign-in help',
+    header: 'Help: Services',
+    description: 'Here are some common questions related to services.',
+    sections: [
+      {
+        title: 'I\'m having trouble with a service, what can I do?',
+        link: '/services',
+        text: 'Find answers to some of the common issues with using the services hosted within DfE Sign-in.',
+        updated: 'April 2021',
+      },
+      {
+        title: 'How do I request access to a service?',
+        link: '/services',
+        text: 'Learn about the process of requesting a service as an end user, along with how long it should take until your request is approved.',
+        updated: 'April 2021',
+      },
+      {
+        title: 'Do I need access to services to use DfE Sign-in?',
+        link: '/services',
+        text: 'Depending on what you intend to do with DfE Sign-in, you may not need access to any services. Learn more here.',
+        updated: 'April 2021',
+      },
+      {
+        title: 'How do I add a service to my account?',
+        link: '/services',
+        text: 'If you\'re an approver, you can add a service to your account quite easily. Find out more here.',
+        updated: 'April 2021',
+      },
+      {
+        title: 'Profile',
+        link: '/services',
+        text: 'Find help with your profile, including how to change your email address and password.',
+        updated: 'April 2021',
+      },
+      {
+        title: 'Approvers',
+        link: '/services',
+        text: 'Find help with approver-related questions, including what an approver is and what they do.',
+        updated: 'April 2021',
+      },
+      {
+        title: 'End users',
+        link: '/services',
+        text: 'Find help with end user-related questions, including how to add a service to an end user.',
+        updated: 'April 2021',
+      },
+    ],
+    relatedArticles: {
+      title: 'Most popular help topics',
+      items: [
+        {
+          text: 'How do I approve a request?',
+          link: '/services',
+        },
+        {
+          text: 'What is an approver?',
+          link: '/services',
+        },
+        {
+          text: 'How do I request access to a service?',
+          link: '/services',
+        },
+        {
+          text: 'How do I add a service to my account?',
+          link: '/services',
+        },
+        {
+          text: 'Do I need a separate email address for each organisation?',
+          link: '/services',
+        },
+      ],
+    },
   };
-  return res.render('services/views/services', model);
+  return res.render('dashboard/views/dashboard', model);
 };
 
 module.exports = {
