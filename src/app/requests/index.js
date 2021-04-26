33856b2891910650cb9357d0fb7e9b936a845a6a
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2021-04-26 14:28:46 +0100

    Added how to approve requests page

--- src/app/requests/index.js
+++ src/app/requests/index.js
@@ -7,6 +7,7 @@ const { asyncWrapper } = require('login.dfe.express-error-handling');
 const { get: getRequests } = require('./requests');
 const { get: getAccessToServices } = require('../requests/accessToServices');
 const { get: getHowToReject } = require('../requests/howToReject');
+const { get: getHowToApprove } = require('../requests/howToApprove');
 
 const router = express.Router({ mergeParams: true });
 
@@ -15,6 +16,7 @@ const routes = (csrf) => {
   router.get('/', csrf, asyncWrapper(getRequests));
   router.get('/access-to-services', csrf, asyncWrapper(getAccessToServices));
   router.get('/how-to-reject', csrf, asyncWrapper(getHowToReject));
+  router.get('/how-to-approve', csrf, asyncWrapper(getHowToApprove));
 
   // add all other routes to new pages here
 
