460bff39a94ae244dcdd20cf076eae0cb7a2cf74
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2021-04-26 16:42:37 +0100

    Change email in my account page

--- src/app/profile/index.js
+++ src/app/profile/index.js
@@ -7,6 +7,7 @@ const { asyncWrapper } = require('login.dfe.express-error-handling');
 const { get: getProfile } = require('./profile');
 const { get: getSetupAccount } = require('./setupAccount');
 const { get: getChangePassword } = require('./changePassword');
+const { get: getChangeEmail } = require('./changeEmail');
 
 const router = express.Router({ mergeParams: true });
 
@@ -15,6 +16,7 @@ const routes = (csrf) => {
   router.get('/', csrf, asyncWrapper(getProfile));
   router.get('/setup-account', csrf, asyncWrapper(getSetupAccount));
   router.get('/change-password', csrf, asyncWrapper(getChangePassword));
+  router.get('/change-email', csrf, asyncWrapper(getChangeEmail));
 
   // add all other routes to new pages here
 
