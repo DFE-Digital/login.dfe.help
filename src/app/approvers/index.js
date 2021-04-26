8713a1752c90d8c0a9306aa0ad208ac1bfd3e415
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2021-04-26 16:57:21 +0100

    Make someone an approver page

--- src/app/approvers/index.js
+++ src/app/approvers/index.js
@@ -5,12 +5,14 @@ const express = require('express');
 const { asyncWrapper } = require('login.dfe.express-error-handling');
 
 const { get: getApprovers } = require('./approvers');
+const { get: getMakeSomeoneApprover } = require('./makeSomeoneApprover');
 
 const router = express.Router({ mergeParams: true });
 
 const routes = (csrf) => {
 
   router.get('/', csrf, asyncWrapper(getApprovers));
+  router.get('/make-someone-approver', csrf, asyncWrapper(getMakeSomeoneApprover));
 
   // add all other routes to new pages here
 
