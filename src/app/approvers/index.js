3f29f096bb6fd8babea031b925e6dce53cc048f0
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2021-04-27 10:35:01 +0100

    what is an approver and make someone approver pages

--- src/app/approvers/index.js
+++ src/app/approvers/index.js
@@ -6,6 +6,7 @@ const { asyncWrapper } = require('login.dfe.express-error-handling');
 
 const { get: getApprovers } = require('./approvers');
 const { get: getMakeSomeoneApprover } = require('./makeSomeoneApprover');
+const { get: getWhatIsApprover } = require('./whatIsApprover');
 
 const router = express.Router({ mergeParams: true });
 
@@ -13,6 +14,7 @@ const routes = (csrf) => {
 
   router.get('/', csrf, asyncWrapper(getApprovers));
   router.get('/make-someone-approver', csrf, asyncWrapper(getMakeSomeoneApprover));
+  router.get('/what-is-approver', csrf, asyncWrapper(getWhatIsApprover));
 
   // add all other routes to new pages here
 
