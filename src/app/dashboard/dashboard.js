bad3c9e87c58bd685a81de29ef01f7df3b8da562
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2021-04-27 16:27:24 +0100

    Contact us page with notification in dashboard

--- src/app/dashboard/dashboard.js
+++ src/app/dashboard/dashboard.js
@@ -1,8 +1,9 @@
-const get = async (req, res) => {
+const get = async (req, res, showContactFormSubmitted) => {
   const model = {
     csrfToken: req.csrfToken(),
     title: 'DfE Sign-in help',
     backLink: false,
+    showContactFormSubmitted,
   };
   return res.render('dashboard/views/dashboard', model);
 };
