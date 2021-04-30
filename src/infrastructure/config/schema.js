c0974b57fa73d50aeb9a98603b5516d819649573
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2021-04-30 15:41:42 +0100

    Initial version with authentication as it is in services

--- src/infrastructure/config/schema.js
+++ src/infrastructure/config/schema.js
@@ -7,12 +7,38 @@ const notificationsSchema = new SimpleSchema({
   connectionString: patterns.redis,
 });
 
+const identifyingPartySchema = new SimpleSchema({
+  url: patterns.url,
+  clientId: String,
+  clientSecret: String,
+  clockTolerance: SimpleSchema.Integer,
+});
+
+const adapterSchema = new SimpleSchema({
+  type: {
+    type: String,
+    allowedValues: ['file', 'redis', 'mongo', 'azuread', 'sequelize'],
+  },
+  directories: {
+    type: schemas.sequelizeConnection,
+    optional: true,
+  },
+  organisation: {
+    type: schemas.sequelizeConnection,
+    optional: true,
+  },
+});
+
 const schema = new SimpleSchema({
   loggerSettings: schemas.loggerSettings,
   hostingEnvironment: schemas.hostingEnvironment,
   applications: schemas.apiClient,
   notifications: notificationsSchema,
   assets: schemas.assets,
+  identifyingParty: identifyingPartySchema,
+  organisations: schemas.apiClient,
+  adapter: adapterSchema,
+  database: schemas.sequelizeConnection,
 });
 
 module.exports.validate = () => {
--- src/routes.js
+++ src/routes.js
@@ -10,28 +10,75 @@ const approvers = require('./app/approvers');
 const endUsers = require('./app/endUsers');
 const contactUs = require('./app/contactUs');
 
+const passport = require('passport');
+const signOut = require('./app/signOut');
+const logger = require('./infrastructure/logger');
+const { isLoggedIn } = require('./infrastructure/utils');
+
 const mountRoutes = (app, csrf) => {
+
+  // authentication routes
+  app.get('/auth', passport.authenticate('oidc'));
+  app.get('/auth/cb', (req, res, next) => {
+    const defaultLoggedInPath = '/dashboard';
+
+    if (req.query.error === 'sessionexpired') {
+      return res.redirect(defaultLoggedInPath);
+    }
+    passport.authenticate('oidc', (err, user) => {
+      let redirectUrl = defaultLoggedInPath;
+
+      if (err) {
+        if (err.message.match(/state\smismatch/)) {
+          req.session = null;
+          return res.redirect(defaultLoggedInPath);
+        }
+        logger.error(`Error in auth callback - ${err}`);
+        return next(err);
+      }
+      if (!user) {
+        return res.redirect('/');
+      }
+
+      if (req.session.redirectUrl) {
+        redirectUrl = req.session.redirectUrl;
+        req.session.redirectUrl = null;
+      }
+
+      return req.logIn(user, (loginErr) => {
+        if (loginErr) {
+          logger.error(`Login error in auth callback - ${loginErr}`);
+          return next(loginErr);
+        }
+        if (redirectUrl.endsWith('signout/complete')) redirectUrl = '/';
+        return res.redirect(redirectUrl);
+      });
+    })(req, res, next);
+  });
+
+  app.use('/signout', signOut(csrf));
+
+  // app routes
   app.use('/healthcheck', healthCheck({ config }));
 
   // route for the dashboard
   // keeping original route to help page to avoid having to update all links in footers
-  app.use('/contact', dashboard(csrf));
-  app.use('/dashboard', dashboard(csrf));
+  app.use('/contact', isLoggedIn, dashboard(csrf));
+  app.use('/dashboard', isLoggedIn, dashboard(csrf));
 
-  // mount additional routes for services, approvers, etc
-  app.use('/services', services(csrf));
-  app.use('/organisations', organisations(csrf));
-  app.use('/manage-users', manageUsers(csrf));
-  app.use('/requests', requests(csrf));
-  app.use('/profile', profile(csrf));
-  app.use('/approvers', approvers(csrf));
-  app.use('/end-users', endUsers(csrf));
-  app.use('/contact-us', contactUs(csrf));
+  // additional routes for services, approvers, etc
+  app.use('/services', isLoggedIn, services(csrf));
+  app.use('/organisations', isLoggedIn, organisations(csrf));
+  app.use('/manage-users', isLoggedIn, manageUsers(csrf));
+  app.use('/requests', isLoggedIn, requests(csrf));
+  app.use('/profile', isLoggedIn, profile(csrf));
+  app.use('/approvers', isLoggedIn, approvers(csrf));
+  app.use('/end-users', isLoggedIn, endUsers(csrf));
+  app.use('/contact-us', isLoggedIn, contactUs(csrf));
 
   app.get('*', (req, res) => {
     res.redirect('/dashboard');
   });
-
 };
 
 module.exports = mountRoutes;
