c8d9feb0f5ac86a38afbec2b8226232ee9e9a409
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2021-04-30 15:41:42 +0100

    Initial version with authentication as it is in services

---
+++ src/infrastructure/oidc/index.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-param-reassign */
+const config = require('../config');
+const { Strategy, Issuer } = require('openid-client');
+const logger = require('../logger');
+const asyncRetry = require('login.dfe.async-retry');
+
+// passport strategy for openid-client
+const getPassportStrategy = async () => {
+  Issuer.defaultHttpOptions = { timeout: 10000 };
+  const issuer = await asyncRetry(
+    async () => await Issuer.discover(config.identifyingParty.url),
+    asyncRetry.strategies.apiStrategy,
+  );
+  const client = new issuer.Client({
+    client_id: config.identifyingParty.clientId,
+    client_secret: config.identifyingParty.clientSecret,
+  });
+  if (config.identifyingParty.clockTolerance && config.identifyingParty.clockTolerance > 0) {
+    client.CLOCK_TOLERANCE = config.identifyingParty.clockTolerance;
+  }
+
+  return new Strategy(
+    {
+      client,
+      params: {
+        redirect_uri: `${config.hostingEnvironment.protocol}://${config.hostingEnvironment.host}:${config.hostingEnvironment.port}/auth/cb`,
+        scope: 'openid profile email',
+      },
+    },
+    (tokenset, authUserInfo, done) => {
+      client
+        .userinfo(tokenset.access_token)
+        .then((userInfo) => {
+          userInfo.id = userInfo.sub;
+          userInfo.name = userInfo.sub;
+          userInfo.id_token = tokenset.id_token;
+
+          done(null, userInfo);
+        })
+        .catch((err) => {
+          logger.error(err);
+          done(err);
+        });
+    },
+  );
+};
+
+module.exports = getPassportStrategy;
---
+++ src/infrastructure/utils/index.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const config = require('./../config');
+const { getOrganisationAndServiceForUserV2, getAllRequestsForApprover } = require('./../organisations');
+
+const isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated() || req.originalUrl.startsWith('/signout?redirected=true')) {
+    res.locals.isLoggedIn = true;
+    return next();
+  }
+  req.session.redirectUrl = req.originalUrl;
+  return res.status(302).redirect('/auth');
+};
+
+const isApprover = (req, res, next) => {
+  if (req.userOrganisations) {
+    const userApproverOrgs = req.userOrganisations.filter((x) => x.role.id === 10000);
+    if (userApproverOrgs.find((x) => x.organisation.id.toLowerCase() === req.params.orgId.toLowerCase())) {
+      return next();
+    }
+  }
+  return res.status(401).render('errors/views/notAuthorised');
+};
+
+const getUserEmail = (user) => user.email || '';
+
+const getUserDisplayName = (user) => `${user.given_name || ''} ${user.family_name || ''}`.trim();
+
+const setUserContext = async (req, res, next) => {
+  if (req.user) {
+    res.locals.user = req.user;
+    res.locals.displayName = getUserDisplayName(req.user);
+    const organisations = await getOrganisationAndServiceForUserV2(req.user.sub, req.id);
+    req.userOrganisations = organisations;
+    try {
+      if (req.userOrganisations) {
+        res.locals.isApprover = req.userOrganisations.filter((x) => x.role.id === 10000).length > 0;
+      }
+      if (res.locals.isApprover) {
+        const approverOrgRequests = await getAllRequestsForApprover(req.user.sub, req.id);
+        req.organisationRequests = approverOrgRequests;
+      }
+    } catch (e) {
+      return e;
+    }
+  }
+  next();
+};
+
+const asyncMiddleware = (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next);
+};
+
+const setConfigContext = (req, res, next) => {
+  res.locals.profilesUrl = config.hostingEnvironment.profileUrl;
+  next();
+};
+
+const mapUserStatus = (status, changedOn = null) => {
+  // TODO: use userStatusMap
+  if (status === -2) {
+    return { id: -2, description: 'Deactivated Invitation', changedOn };
+  }
+  if (status === -1) {
+    return { id: -1, description: 'Invited', changedOn };
+  }
+  if (status === 0) {
+    return { id: 0, description: 'Deactivated', changedOn };
+  }
+  return { id: 1, description: 'Active', changedOn };
+};
+
+module.exports = {
+  isLoggedIn,
+  getUserEmail,
+  getUserDisplayName,
+  setUserContext,
+  asyncMiddleware,
+  setConfigContext,
+  isApprover,
+  mapUserStatus,
+};
