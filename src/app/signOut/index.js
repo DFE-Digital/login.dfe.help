69d19dcbdb52fbea244fe564e7ecf63816e847ca
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2021-05-07 09:58:19 +0100

    Remove unnecessary service to get postlogout redirect urls

--- src/app/signOut/index.js
+++ src/app/signOut/index.js
@@ -12,6 +12,7 @@ const router = express.Router({ mergeParams: true });
 const signout = () => {
   logger.info('Mounting signOut route');
   router.get('/', asyncWrapper(signOutUser));
+  // TODO Check if we really need this other route?
   router.get('/complete', asyncWrapper(complete));
   return router;
 };
