996e9aae3be47359569c7a1c5bc8bcd2d536328e
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2021-05-07 16:54:45 +0100

    Removed comment

--- src/app/signOut/index.js
+++ src/app/signOut/index.js
@@ -12,7 +12,6 @@ const router = express.Router({ mergeParams: true });
 const signout = () => {
   logger.info('Mounting signOut route');
   router.get('/', asyncWrapper(signOutUser));
-  // TODO Check if we really need this other route?
   router.get('/complete', asyncWrapper(complete));
   return router;
 };
