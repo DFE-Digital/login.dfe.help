be22581dca682bd7df19b7b7a7f11f09e0e02aa7
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2021-05-21 16:03:05 +0100

    Using redirect URI to send users back to original sign in after contact us form sent

--- src/app/contactUs/getContactUsCompleted.js
+++ src/app/contactUs/getContactUsCompleted.js
@@ -1,9 +1,12 @@
 const get = async (req, res) => {
+  // link to go back to sign in (if user not authenticated) will point to either redirectUri or services page
+  const redirectUri = req.query.redirect_uri;
   res.render('contactUs/views/contactUsCompleted', {
     csrfToken: req.csrfToken(),
     title: 'DfE Sign-in help',
     isHidden: true,
     isHomeTopHidden: true,
+    redirectUri,
   });
 };
 
