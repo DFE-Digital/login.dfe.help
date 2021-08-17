9aa395b8b7c2a20568d8c35cbda6eb7ddbab0d4c
mukeshrawat02 <mukeshrawat02@gmail.com>
2021-08-17 09:11:21 +0100

    Added new help page and content for request a service

--- src/app/requests/index.js
+++ src/app/requests/index.js
@@ -6,6 +6,7 @@ const { asyncWrapper } = require('login.dfe.express-error-handling');
 const { get: getRequests } = require('./requests');
 const { get: getHowToReject } = require('./howToReject');
 const { get: getHowToApprove } = require('./howToApprove');
+const { get: getCanEndUserRequestService } = require('./canEndUserRequestService');
 
 const router = express.Router({ mergeParams: true });
 
@@ -14,6 +15,7 @@ const routes = (csrf) => {
   router.get('/', csrf, asyncWrapper(getRequests));
   router.get('/how-to-reject', csrf, asyncWrapper(getHowToReject));
   router.get('/how-to-approve', csrf, asyncWrapper(getHowToApprove));
+  router.get('/can-end-user-request-service', csrf, asyncWrapper(getCanEndUserRequestService));
 
   return router;
 };
