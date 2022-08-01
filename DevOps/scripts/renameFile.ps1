1e95318985d5495b2eb84da878947e17654075b8
Univer-lion-lab <rafael.tsiakkouris@methods.co.uk>
2022-08-01 13:49:00 +0100

    Update renameFile.ps1

--- DevOps/scripts/renameFile.ps1
+++ DevOps/scripts/renameFile.ps1
@@ -1,8 +1,8 @@
 param ($applicationName, $environmentName)
-$fileName = ".\Publish\config\login.dfe.$(applicationName).$(environmentName).json"
+$fileName = ".\Publish\config\login.dfe.$applicationName.$environmentName.json"
     if (Test-Path $fileName) {
         write-host existing config found, deleting
         Remove-Item $fileName
     }
-write-host renaming pre-built config for $(applicationName) [$(environmentName)]
-Rename-Item -path .\Publish\config\$(applicationName)-standalone.json -newname $fileName
+write-host renaming pre-built config for $applicationName [$environmentName]
+Rename-Item -path .\Publish\config\$applicationName-standalone.json -newname $fileName
