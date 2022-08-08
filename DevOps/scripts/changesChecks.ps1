ce5f32f13518ed498bc7dba833bed78d46ba0981
Univer-lion-lab <rafael.tsiakkouris@methods.co.uk>
2022-08-08 12:42:40 +0100

    Update changesChecks.ps1

--- DevOps/scripts/changesChecks.ps1
+++ DevOps/scripts/changesChecks.ps1
@@ -4,6 +4,7 @@ $editedFiles = git diff HEAD HEAD~ --name-only
 Write-Output "Infr Value: $($env:InfrDeploy)"
 Write-Output "App Value: $($env:AppDeploy)"
 $editedFiles | ForEach-Object {
+    Write-Output $_
     Switch -Wildcard ($_ ) {
         'DevOps/*' { Write-Host "##vso[task.setvariable variable=InfrDeploy;isOutput=true]True"}
         'src/*' { Write-Host "##vso[task.setvariable variable=AppDeploy;isOutput=true]True"  }
