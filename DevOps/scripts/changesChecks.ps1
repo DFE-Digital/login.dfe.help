d3d453de16edb774f82072dcb90e165a460ae9ae
Univer-lion-lab <rafael.tsiakkouris@methods.co.uk>
2022-08-08 12:37:48 +0100

    Update changesChecks.ps1

--- DevOps/scripts/changesChecks.ps1
+++ DevOps/scripts/changesChecks.ps1
@@ -1,14 +1,14 @@
-Write-Host "##vso[task.setvariable variable=InfrDeploy]False"
-Write-Host "##vso[task.setvariable variable=AppDeploy]False"
+Write-Host "##vso[task.setvariable variable=InfrDeploy;isOutput=true]False"
+Write-Host "##vso[task.setvariable variable=AppDeploy;isOutput=true]False"
 $editedFiles = git diff HEAD HEAD~ --name-only
 Write-Output "Infr Value: $($env:InfrDeploy)"
 Write-Output "App Value: $($env:AppDeploy)"
 $editedFiles | ForEach-Object {
     Switch -Wildcard ($_ ) {
-        'DevOps/*' { Write-Host "##vso[task.setvariable variable=InfrDeploy]True"}
-        'src/*' { Write-Host "##vso[task.setvariable variable=AppDeploy]True"  }
-        'test/*' { Write-Host "##vso[task.setvariable variable=AppDeploy]True"}
-        'package*' { Write-Host "##vso[task.setvariable variable=AppDeploy]True"}
+        'DevOps/*' { Write-Host "##vso[task.setvariable variable=InfrDeploy;isOutput=true]True"}
+        'src/*' { Write-Host "##vso[task.setvariable variable=AppDeploy;isOutput=true]True"  }
+        'test/*' { Write-Host "##vso[task.setvariable variable=AppDeploy;isOutput=true]True"}
+        'package*' { Write-Host "##vso[task.setvariable variable=AppDeploy;isOutput=true]True"}
         # The rest of your path filters
     }
 
