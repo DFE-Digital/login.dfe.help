cf506d361b1dbe639c905f13e75da4c86493d310
Univer-lion-lab <rafael.tsiakkouris@methods.co.uk>
2022-08-11 11:39:00 +0100

    file transfer to central repo

--- DevOps/scripts/renameFile.ps1
+++
@@ -1,8 +0,0 @@
-param ($applicationName, $environmentName, $directory)
-$fileName = "$directory\Publish\config\login.dfe.$applicationName.$environmentName.json"
-    if (Test-Path $fileName) {
-        write-host existing config found, deleting
-        Remove-Item $fileName
-    }
-write-host renaming pre-built config for $applicationName [$environmentName]
-Rename-Item -path $directory\Publish\config\$applicationName-standalone.json -newname $fileName
