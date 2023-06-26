677f2c4d385e7c8a27d88f7ad9a03151a2890973
stottidfe <peter.stott@education.gov.uk>
2023-06-26 14:50:36 +0100

    added unit test for new page

---
+++ test/app/manageUsers/changeSubServiceToUser.get.test.js
@@ -0,0 +1,39 @@
+jest.mock('./../../../src/infrastructure/config', () => require('../../utils').configMockFactory());
+
+const { getRequestMock, getResponseMock } = require('../../utils');
+
+const res = getResponseMock();
+
+describe('when displaying the help page for manageUsers/changeSubServiceToUser', () => {
+  let req;
+  let getDashboard;
+
+  beforeEach(() => {
+    req = getRequestMock();
+    res.mockResetAll();
+    getDashboard = require('../../../src/app/manageUsers/changeSubServiceToUser').get;
+  });
+
+  it('should render the help page for manageUsers/addSubServiceToUser', async () => {
+    await getDashboard(req, res);
+
+    expect(res.render.mock.calls).toHaveLength(1);
+    expect(res.render.mock.calls[0][0]).toBe('manageUsers/views/changeSubServiceToUser');
+  });
+
+  it('should include csrf token', async () => {
+    await getDashboard(req, res);
+
+    expect(res.render.mock.calls[0][1]).toMatchObject({
+      csrfToken: 'token',
+    });
+  });
+
+  it('should include the title', async () => {
+    await getDashboard(req, res);
+
+    expect(res.render.mock.calls[0][1]).toMatchObject({
+      title: 'DfE Sign-in help',
+    });
+  });
+});
