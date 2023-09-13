9150eb163afe4c70889239f4d813ebb1dc3b6d83
stottidfe <peter.stott@education.gov.uk>
2023-09-13 15:06:12 +0100

    subtitle changes

--- src/app/dashboard/dashboard.js
+++ src/app/dashboard/dashboard.js
@@ -2,6 +2,7 @@ const get = async (req, res) => {
   const model = {
     csrfToken: req.csrfToken(),
     title: 'DfE Sign-in',
+    subTitle: 'DfE Sign-in',
     backLink: false,
   };
   return res.render('dashboard/views/dashboard', model);
