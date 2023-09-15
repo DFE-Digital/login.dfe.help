6f9d80cde8a038ba8a37eced0db3fe182363fb01
stottidfe <peter.stott@education.gov.uk>
2023-09-15 17:08:32 +0100

    half in

--- src/app/manageConsole/index.js
+++ src/app/manageConsole/index.js
@@ -2,12 +2,13 @@
 
 const express = require('express');
 const { asyncWrapper } = require('login.dfe.express-error-handling');
-
+const { get: getManageDashboard } = require('../dashboard/managDashboard');
 const { get: gethowtoEditServiceConfig } = require('./howtoEditServiceConfig');
 
 const router = express.Router({ mergeParams: true });
 
 const routes = (csrf) => {
+  router.get('/', csrf, asyncWrapper(getManageDashboard));
   router.get('/how-to-edit-service-config', csrf, asyncWrapper(gethowtoEditServiceConfig));
 
   return router;
