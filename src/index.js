30e763ff4168c0e3dcce564fc7def41544d58264
stottidfe <peter.stott@education.gov.uk>
2023-09-19 15:44:12 +0100

    update links

--- src/index.js
+++ src/index.js
@@ -117,7 +117,7 @@ const init = async () => {
       interactions: config.hostingEnvironment.interactionsUrl,
       services: config.hostingEnvironment.servicesUrl,
       profile: config.hostingEnvironment.profileUrl,
-      manageConsole:  config.hostingEnvironment.manageConsoleUrl,
+      manageConsole: config.hostingEnvironment.manageConsoleUrl,
       assets: assetsUrl,
       survey: config.hostingEnvironment.surveyUrl,
     },
