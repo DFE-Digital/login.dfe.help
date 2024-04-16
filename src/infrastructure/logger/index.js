358b1463d13370e088ae2e3e8be4ffb5bb8b8bed
Jared Marsh <jared.marsh@education.gov.uk>
2024-04-16 19:06:30 +0100

    Ensure console and app insights loggers do not log audit level records

--- src/infrastructure/logger/index.js
+++ src/infrastructure/logger/index.js
@@ -1,13 +1,12 @@
 'use strict';
 
 const winston = require('winston');
-const config = require('./../config');
 const AuditTransporter = require('login.dfe.audit.transporter');
 const appInsights = require('applicationinsights');
 const AppInsightsTransport = require('login.dfe.winston-appinsights');
+const config = require('../config');
 
-const logLevel =
-  config && config.loggerSettings && config.loggerSettings.logLevel ? config.loggerSettings.logLevel : 'info';
+const logLevel = (config && config.loggerSettings && config.loggerSettings.logLevel) ? config.loggerSettings.logLevel : 'info';
 
 const customLevels = {
   levels: {
@@ -19,20 +18,20 @@ const customLevels = {
     debug: 5,
     silly: 6,
   },
-  colors: {
-    info: 'yellow',
-    ok: 'green',
-    error: 'red',
-    audit: 'magenta',
-  },
 };
+
+// Formatter to hide audit records from other loggers.
+const hideAudit = winston.format((info) => ((info.level.toLowerCase() === 'audit') ? false : info));
 
 const loggerConfig = {
   levels: customLevels.levels,
   transports: [],
 };
 
-// loggerConfig.transports.push(new winston.transports.Console({ level: logLevel, colorize: true }));
+loggerConfig.transports.push(new winston.transports.Console({
+  format: winston.format.combine(hideAudit(), winston.format.simple()),
+  level: logLevel,
+}));
 
 if (config && config.loggerSettings && config.loggerSettings.redis && config.loggerSettings.redis.enabled) {
   loggerConfig.transports.push(
@@ -55,14 +54,13 @@ if (auditTransport) {
 
 if (config.hostingEnvironment.applicationInsights) {
   appInsights.setup(config.hostingEnvironment.applicationInsights).setAutoCollectConsole(false, false).start();
-  loggerConfig.transports.push(
-    new AppInsightsTransport({
-      client: appInsights.defaultClient,
-      applicationName: config.loggerSettings.applicationName || 'Services',
-      type: 'event',
-      treatErrorsAsExceptions: true,
-    }),
-  );
+  loggerConfig.transports.push(new AppInsightsTransport({
+    format: winston.format.combine(hideAudit(), winston.format.json()),
+    client: appInsights.defaultClient,
+    applicationName: config.loggerSettings.applicationName || 'Help',
+    type: 'event',
+    treatErrorsAsExceptions: true,
+  }));
 }
 
 const logger = winston.createLogger(loggerConfig);
