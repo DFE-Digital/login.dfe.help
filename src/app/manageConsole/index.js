c21a6fa6bc01289243e2fed4f3491bb84b9c8b94
Richard Ward <richard.ward@methods.co.uk>
2024-10-21 16:05:44 +0100

    DSI-6538: add help for manage console

--- src/app/manageConsole/index.js
+++ src/app/manageConsole/index.js
@@ -4,12 +4,14 @@ const express = require('express');
 const { asyncWrapper } = require('login.dfe.express-error-handling');
 const { get: getManageDashboard } = require('../dashboard/managDashboard');
 const { get: gethowtoEditServiceConfig } = require('./howtoEditServiceConfig');
+const { get: aboutManageConsole } = require('./aboutManageConsole');
 
 const router = express.Router({ mergeParams: true });
 
 const routes = (csrf) => {
   router.get('/:sid', csrf, asyncWrapper(getManageDashboard));
   router.get('/:sid/how-to-edit-service-config', csrf, asyncWrapper(gethowtoEditServiceConfig));
+  router.get('/:sid/about-manage-console', csrf, asyncWrapper(aboutManageConsole));
 
   return router;
 };
