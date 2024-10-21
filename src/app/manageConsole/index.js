1c61f1e430132e13a64099b3af8fed9f8f66b10c
Julio Velezmoro <julio28ve@gmail.com>
2024-10-21 09:33:34 +0100

    create how to manage users page

--- src/app/manageConsole/index.js
+++ src/app/manageConsole/index.js
@@ -4,12 +4,14 @@ const express = require('express');
 const { asyncWrapper } = require('login.dfe.express-error-handling');
 const { get: getManageDashboard } = require('../dashboard/managDashboard');
 const { get: gethowtoEditServiceConfig } = require('./howtoEditServiceConfig');
+const { get: gethowtoManageUsers } = require('./howtoManageUsers');
 
 const router = express.Router({ mergeParams: true });
 
 const routes = (csrf) => {
   router.get('/:sid', csrf, asyncWrapper(getManageDashboard));
   router.get('/:sid/how-to-edit-service-config', csrf, asyncWrapper(gethowtoEditServiceConfig));
+  router.get('/:sid/how-to-manage-users', csrf, asyncWrapper(gethowtoManageUsers));
 
   return router;
 };
