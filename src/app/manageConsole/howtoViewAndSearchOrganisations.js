9f8918b12dcaf9ee9eac7e450378b76a1b21c7d1
Julio Velezmoro <julio28ve@gmail.com>
2024-10-21 16:55:49 +0100

    create how to view and search organisations page

---
+++ src/app/manageConsole/howtoViewAndSearchOrganisations.js
@@ -0,0 +1,29 @@
+const { getSingleUserServiceAndRoles } = require('./utils');
+const config = require('../../infrastructure/config');
+
+const get = async (req, res) => {
+  if (req.params.sid !== undefined && req.isAuthenticated()) {
+    let bkLink = true;
+    let tbLink = '';
+    if (req.headers.referer === undefined) {
+      tbLink = `https://${config.hostingEnvironment.host}:${config.hostingEnvironment.port}/manageConsole/${req.params.sid}`;
+      bkLink = false;
+    }
+    const manageRolesForService = await getSingleUserServiceAndRoles(req);
+    const model = {
+      csrfToken: req.csrfToken(),
+      title: 'DfE Manage',
+      app: { title: 'DfE Sign-in manage console' },
+      subTitle: 'DfE Sign-in manage console',
+      serviceId: req.params.sid,
+      userRoles: manageRolesForService,
+      backLink: bkLink,
+      tabLink: tbLink,
+    };
+    return res.render('manageConsole/views/howtoViewAndSearchOrganisations', model);
+  }
+};
+
+module.exports = {
+  get,
+};
--- src/app/manageConsole/index.js
+++ src/app/manageConsole/index.js
@@ -5,11 +5,15 @@ const { asyncWrapper } = require('login.dfe.express-error-handling');
 const { get: getManageDashboard } = require('../dashboard/managDashboard');
 const { get: gethowtoEditServiceConfig } = require('./howtoEditServiceConfig');
 
+const { get: gethowtoViewAndSearchOrganisations } = require('./howtoViewAndSearchOrganisations')
+
 const router = express.Router({ mergeParams: true });
 
 const routes = (csrf) => {
   router.get('/:sid', csrf, asyncWrapper(getManageDashboard));
   router.get('/:sid/how-to-edit-service-config', csrf, asyncWrapper(gethowtoEditServiceConfig));
+
+  router.get('/:sid/how-to-view-and-search-organisations', csrf, asyncWrapper(gethowtoViewAndSearchOrganisations));
 
   return router;
 };
