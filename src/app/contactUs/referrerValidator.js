573d078d4764f577b12f3d955b015079ab69af45
Richard Ward <richard.ward@methods.co.uk>
2024-12-06 11:53:17 +0000

    Provide a referrer validator method and tests to go with.

---
+++ src/app/contactUs/referrerValidator.js
@@ -0,0 +1,11 @@
+
+
+function isValidReferrer(referrer) {
+  try {
+    return ['http:', 'https:'].includes((new URL(referrer)).protocol);
+  } catch {
+    return referrer?.startsWith('/') ?? false;
+  }
+}
+
+module.exports = isValidReferrer;
