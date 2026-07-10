6b25034fc4db61c7783670892369ab7b3d93b464
vsolomonadv <victor.solomon@advania.co.uk>
2026-07-10 09:45:30 +0100

    refactor(NSA-9688): help/utils.js uses isHiddenForHelp from API (#321)
    * refactor(NSA-9688): help/utils.js uses isHiddenForHelp from API
    
    * fix(NSA-9688): use correct fallback shape when listAllServices returns falsy

--- src/app/shared/utils.js
+++ src/app/shared/utils.js
@@ -3,15 +3,11 @@ const sortBy = require("lodash/sortBy");
 const uniqBy = require("lodash/uniqBy");
 
 const getAndMapExternalServices = async (correlationId) => {
-  const allServices = (await listAllServices(correlationId)) || [];
+  const allServices = (await listAllServices(correlationId)) || {
+    services: [],
+  };
   const externalServices = allServices.services.filter(
-    (x) =>
-      x.isExternalService === true &&
-      !(
-        x.relyingParty &&
-        x.relyingParty.params &&
-        x.relyingParty.params.helpHidden === "true"
-      ),
+    (x) => x.isExternalService === true && !x.isHiddenForHelp,
   );
   const services = uniqBy(
     externalServices.map((service) => ({
